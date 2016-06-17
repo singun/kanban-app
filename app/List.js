8b8d91c2513ec06f6c2ba8dc3329ae0f59a90c9e
daewook.laptop <daeuky1@gmail.com>
2016-06-18 00:02:35 +0900

    2장 끝 - task 입력 폼 추가 및 style 적용

--- app/List.js
+++ app/List.js
@@ -8,6 +8,7 @@ class List extends Component {
             return <Card key={card.id}
                         id={card.id}
                         title={card.title}
+                        color={card.color}
                         description={card.description}
                         tasks={card.tasks} />
         });

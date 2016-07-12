462bd794ac51ec7adcc7f092e7f36d53bc7ab65c
Woogi(신대욱) <daeuky@coupang.com>
2016-07-12 23:58:23 +0900

    ch6 flux example

--- app-flux/App.js
+++ app-flux/App.js
@@ -1,5 +1,67 @@
+// -------------------------------------------------
+// App version 1
+// -------------------------------------------------
+// import React, { Component } from 'react';
+// import { render } from 'react-dom';
+// import BankBalanceStore from './BankBalanceStore';
+// import BankActions from './BankActions';
+//
+// class App extends Component {
+//   constructor() {
+//     super(...arguments);
+//     BankActions.createAccount();
+//     this.state = {
+//       balance: BankBalanceStore.getState()
+//     };
+//   }
+//
+//   componentDidMount() {
+//     this.storeSubscription = BankBalanceStore.addListener(
+//       data => this.handleStoreChange(data)
+//     );
+//   }
+//
+//   // componentWillMount() {
+//   //   this.storeSubscription.remove();
+//   // }
+//
+//   handleStoreChange() {
+//     this.setState({
+//       balance: BankBalanceStore.getState()
+//     });
+//   }
+//
+//   deposit() {
+//     BankActions.depositIntoAccount(Number(this.refs.amount.value));
+//     this.refs.amount.value = '';
+//   }
+//
+//   withdraw() {
+//     BankActions.withDrawFromAccount(Number(this.refs.amount.value));
+//     this.refs.amount.value = '';
+//   }
+//
+//   render() {
+//     return(
+//       <div>
+//         <header>FlusTrust Bank</header>
+//         <h1>Your balance is ${(this.state.balance).toFixed(2)}</h1>
+//         <div className="atm">
+//           <input type="text" placeholder="Enter Amount" ref="amount" />
+//           <br />
+//           <button onClick={this.withdraw.bind(this)}>Withdraw</button>
+//           <button onClick={this.deposit.bind(this)}>Deposit</button>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+//
+// render(<App />, document.getElementById('root'));
+
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { Container } from 'flux/utils';
 import BankBalanceStore from './BankBalanceStore';
 import BankActions from './BankActions';
 
@@ -7,25 +69,6 @@ class App extends Component {
   constructor() {
     super(...arguments);
     BankActions.createAccount();
-    this.state = {
-      balance: BankBalanceStore.getState()
-    };
-  }
-
-  componentDidMount() {
-    this.storeSubscription = BankBalanceStore.addListener(
-      data => this.handleStoreChange(data)
-    );
-  }
-
-  // componentWillMount() {
-  //   this.storeSubscription.remove();
-  // }
-
-  handleStoreChange() {
-    this.setState({
-      balance: BankBalanceStore.getState()
-    });
   }
 
   deposit() {
@@ -39,9 +82,9 @@ class App extends Component {
   }
 
   render() {
-    return(
+    return (
       <div>
-        <header>FlusTrust Bank</header>
+        <header>FluxTrust Bank</header>
         <h1>Your balance is ${(this.state.balance).toFixed(2)}</h1>
         <div className="atm">
           <input type="text" placeholder="Enter Amount" ref="amount" />
@@ -50,8 +93,14 @@ class App extends Component {
           <button onClick={this.deposit.bind(this)}>Deposit</button>
         </div>
       </div>
-    );
+    )
   }
 }
 
-render(<App />, document.getElementById('root'));
+App.getStores = () => ([BankBalanceStore]);
+App.calculateState = (prevState) => ({
+  balance: BankBalanceStore.getState()
+});
+
+const AppContainer = Container.create(App);
+render(<AppContainer />, document.getElementById('root'));
--- app-flux/BankBalanceStore.js
+++ app-flux/BankBalanceStore.js
@@ -38,31 +38,64 @@
 //
 // export default BankBalanceStore;
 
+
+// -------------------------------------------------
+// Store version 2 - Store in flux/utils
+// -------------------------------------------------
+// import AppDispatcher from './AppDispatcher';
+// import { Store } from 'flux/utils';
+// import bankConstants from './constants';
+//
+// let balance = 0;
+//
+// class BankBalanceStore extends Store {
+//   getState() {
+//     return balance;
+//   }
+//
+//   __onDispatch(action) {
+//     switch (action.type) {
+//       case bankConstants.CREATED_ACCOUNT:
+//         balance = 0;
+//         this.__emitChange();
+//         break;
+//       case bankConstants.DEPOSITED_INTO_ACCOUNT:
+//         balance = balance + action.amount;
+//         this.__emitChange();
+//         break;
+//       case bankConstants.CREATED_ACCOUNT:
+//         balance = balance - action.amount;
+//         this.__emitChange();
+//         break;
+//     }
+//   }
+// }
+//
+// export default new BankBalanceStore(AppDispatcher);
+
+
+// -------------------------------------------------
+// Store version 3 - ReduceStore in flux/utils
+// -------------------------------------------------
 import AppDispatcher from './AppDispatcher';
-import { Store } from 'flux/utils';
 import bankConstants from './constants';
+import { ReduceStore } from 'flux/utils';
 
-let balance = 0;
-
-class BankBalanceStore extends Store {
-  getState() {
-    return balance;
+class BankBalanceStore extends ReduceStore {
+  getInitialState() {
+    return 0;
   }
 
-  __onDispatch(action) {
+  reduce(state, action) {
     switch (action.type) {
       case bankConstants.CREATED_ACCOUNT:
-        balance = 0;
-        this.__emitChange();
-        break;
+        return 0;
       case bankConstants.DEPOSITED_INTO_ACCOUNT:
-        balance = balance + action.amount;
-        this.__emitChange();
-        break;
-      case bankConstants.CREATED_ACCOUNT:
-        balance = balance - action.amount;
-        this.__emitChange();
-        break;
+        return state + action.amount;
+      case bankConstants.WITHDRAW_FROM_ACCOUNT:
+        return state - action.amount;
+      default:
+        return state;
     }
   }
 }

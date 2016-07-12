d7b3bb45f0ad6b089fca502dc0b0c7d832adf84e
Woogi(신대욱) <daeuky@coupang.com>
2016-07-13 00:17:36 +0900

    ch6 async process

--- app-flux/App.js
+++ app-flux/App.js
@@ -63,6 +63,7 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { Container } from 'flux/utils';
 import BankBalanceStore from './BankBalanceStore';
+import BankRewardsStore from './BankRewardsStore';
 import BankActions from './BankActions';
 
 class App extends Component {
@@ -86,6 +87,7 @@ class App extends Component {
       <div>
         <header>FluxTrust Bank</header>
         <h1>Your balance is ${(this.state.balance).toFixed(2)}</h1>
+        <h2>Your Points Rewards Tier is {this.state.rewardsTier}</h2>
         <div className="atm">
           <input type="text" placeholder="Enter Amount" ref="amount" />
           <br />
@@ -97,9 +99,10 @@ class App extends Component {
   }
 }
 
-App.getStores = () => ([BankBalanceStore]);
+App.getStores = () => ([BankBalanceStore, BankRewardsStore]);
 App.calculateState = (prevState) => ({
-  balance: BankBalanceStore.getState()
+  balance: BankBalanceStore.getState(),
+  rewardsTier: BankRewardsStore.getState()
 });
 
 const AppContainer = Container.create(App);
